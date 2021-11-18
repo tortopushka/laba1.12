fdab2196dc78b88615cd44860a871a7bfee9da84
tortopushka <maxdim2@gmail.com>
2021-11-19 01:45:44 +0300

    Initial commit

---
+++ src/main/java/List.java
@@ -0,0 +1,149 @@
+/**
+ * This class allows you to work with data by structuring it into a unidirectional list
+ * @param <T> type of val stored in the list
+ */
+
+public class List<T> {
+    /**
+     * The pointer to the beginning of the list field
+     */
+    private ListElem head;
+    /**
+     * The pointer to the end of the list field
+     */
+    private ListElem tail;
+    /**
+     * The field that stores size of the list
+     */
+    private int size;
+
+    /**
+     * Inner class describing list element
+     */
+    public class ListElem<T> {
+        /**
+         * Element value field
+         */
+        private T data;
+        /**
+         * The pointer to the next item of the list field
+         */
+        private ListElem next;
+        /**
+         * constructor with data
+         *
+         * @param dataElem data added to head type T
+         */
+        ListElem(T dataElem) {
+            data = dataElem;
+            next = null;
+        }
+    }
+    /**
+     * default constructor
+     */
+    public List(){
+        head=null;
+        tail=head;
+        size=0;
+    }
+    /**
+     * check for emptiness
+     *
+     * @return 1 if empty
+     * 0 is not empty
+     */
+    boolean isEmpty(){
+        return(head==null);
+    }
+
+    /**
+     * get size of list
+     * @return size
+     */
+    int getSize(){
+        return size;
+    }
+    /**
+     * add element to List
+     *
+     * @param elem element type T
+     */
+    void add(T elem){
+        ListElem temp=new ListElem(elem);
+        if(tail==null){
+            head=temp;
+            tail=temp;
+        }else{
+            tail.next=temp;
+            tail=temp;
+        }
+        size++;
+    }
+    /**
+     * delete element
+     *
+     * @param index position of deleted element type int
+     * @throws Exception when unavailable position
+     */
+    void delete(int index)throws Exception{
+       if(index<0 || index>=size||size==0){
+            throw new Exception("Exception! Delete an element with an unavailable position!");
+        }
+        else
+        {
+            if (index == 0)
+                deleteFromHead();
+            else
+                deleteFromPosition(index);
+        }
+    }
+    /**
+     * delete element from head of list
+     */
+    private void deleteFromHead(){
+        ListElem temp=head;
+        if (tail==head){
+            tail=null;
+        }
+        head=head.next;
+        temp=null;
+        size--;
+    }
+    /**
+     * delete element from position
+     *
+     * @param pos index of deleted element type int
+     */
+    private void deleteFromPosition(int pos){
+        ListElem temp=head;
+        int index=0;
+        while (index<size && index!=pos-1){
+            index++;
+            temp=temp.next;
+        }
+        ListElem p=temp.next;
+        temp.next=p.next;
+        p=null;
+        size--;
+    }
+    /**
+     * convert list to string
+     *
+     * @return  string
+     */
+    @Override
+    public String toString() {
+        String s = "";
+        ListElem p = head;
+        while (p != null) {
+            if (p.next != null) {
+                s += p.data + "->";
+            } else {
+                s += p.data;
+            }
+            p = p.next;
+        }
+        return s;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+/**
+ * This is my class Main
+ */
+public class Main {
+    public static void main(String[] args){
+        List myList=new List();
+        System.out.println("Введите количество элементов:  ");
+        Scanner in =new Scanner(System.in);
+        while (!in.hasNextInt()){
+            System.out.println("Введите число! ");
+            in.next();
+        }
+        int n=in.nextInt();
+        for(int i=0;i<n;i++){
+            System.out.println((i)+":  ");
+            int val=in.nextInt();
+            myList.add(val);
+        }
+        System.out.println("Список: ");
+        System.out.println(myList.toString());
+        int task=1;
+        System.out.println("Выберите действие: ");
+        while (task!=0){
+            System.out.println(" -------------------------------");
+            System.out.println("0-ВЫХОД ");
+            System.out.println("1-добавить элемент ");
+            System.out.println("2-удалить элемент по индексу");
+            System.out.println("3-вывести размерность ");
+            System.out.println("4-вывод ");
+            System.out.println(" -------------------------------");
+            while (!in.hasNextInt()){
+                System.out.println("Введите число! ");
+                in.next();
+            }
+            task=in.nextInt();
+            if (task<0||task>4) System.out.println("Внимание! Выберите число от 0 до 4!");
+            switch (task){
+                case 0: break;
+                case 1:{
+                    System.out.println("Введите элемент: ");
+                    while (!in.hasNextInt()){
+                        System.out.println("Ошибка! Введите число! ");
+                        in.next();
+                    }
+                    int elem=in.nextInt();
+                    myList.add(elem);
+                    break;
+                }
+                case 2: {
+                    System.out.println("Введите индекс удаляемого элемента: ");
+                    while (!in.hasNextInt()) {
+                        System.out.println("Ошибка! Введите число! ");
+                        in.next();
+                    }
+                    int index = in.nextInt();
+                    try {
+                        myList.delete(index);
+                    } catch (Exception var8) {
+                        System.out.println(var8.getMessage());
+                        System.out.println(myList.toString());
+                    }
+                    break;
+                }
+                case 3:{
+                    System.out.println("Размерность: ");
+                    System.out.println(myList.getSize());
+                    break;
+                }
+                case 4:{
+                    if(myList.isEmpty()){
+                        System.out.println("Список пуст.");
+                    }
+                    else {
+                        System.out.println("Список: ");
+                        System.out.println(myList.toString());
+                    }
+                    break;
+                }
+            }
+        }
+
+    }
+}
